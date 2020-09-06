bae5865b3251fe09b82a1bbb01ec69010d5a5ac9
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2020-09-07 05:16:39 +0900

    FirstCommit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+import settings
+import traceback
+
+DISCORD_TOKEN = settings.DISCORD_TOKEN
+COMMAND_PREFIX = settings.COMMAND_PREFIX
+
+INITIAL_EXETENDIONS = [
+
+]
+
+
+class MyBot(commands.Bot):
+    def __init__(self, command_prefix) -> None:
+        super().__init__(command_prefix)
+
+        for cog in INITIAL_EXETENDIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+
+if __name__ == "__main__":
+    bot = MyBot(command_prefix=COMMAND_PREFIX)
+    bot.run(settings.TOKEN)
