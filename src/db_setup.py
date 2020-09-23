68261b915d72b4ac564f8f0fc297c00b349a080d
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2020-09-23 11:22:19 +0900

    dbテーブル増やす

--- src/db_setup.py
+++ src/db_setup.py
@@ -14,13 +14,13 @@ cur = conn.cursor()
 # DBの中にscheduleテーブルが存在していない場合作成
 cur.execute("SELECT * FROM sqlite_master WHERE type='table' and name='task_schedule'")
 if not cur.fetchone():
-    cur.execute('CREATE TABLE task_schedule  (id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, message_id STRING)')
+    cur.execute('CREATE TABLE task_schedule  (id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, message_id STRING, channel_id STRING)')
 else:
     while True:
         choice = input("テーブルが存在しています。再作成しますか？ [Y/N]: ").lower()
         if choice in ['y', 'ye', 'yes']:
             cur.execute("DROP TABLE task_schedule")
-            cur.execute('CREATE TABLE task_schedule  (id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, message_id STRING)')
+            cur.execute('CREATE TABLE task_schedule  (id INTEGER PRIMARY KEY AUTOINCREMENT, time STRING, message_id STRING, channel_id STRING)')
             print("再作成しました。")
             conn.close()
             exit()
